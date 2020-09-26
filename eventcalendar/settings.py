3172f778976313918650cb8059d33058f4046cb0
anshsrtv <49693160+anshsrtv@users.noreply.github.com>
2020-09-26 19:06:04 +0530

    Update settings.py

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD='Iftomm@123'
-EMAIL_HOST_USER='naughty.ansh007@gmail.com'
+EMAIL_HOST_PASSWORD=''
+EMAIL_HOST_USER=''
